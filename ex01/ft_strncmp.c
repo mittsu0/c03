c054db0fb22f7aa2ba95ec64f917be4c9e0e1e77
mittsu0 <threebridge3824@gmail.com>
2022-03-28 21:45:31 +0900

    submit2

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,12 +6,14 @@
 /*   By: unix_user <unix_user@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 17:54:57 by unix_user         #+#    #+#             */
-/*   Updated: 2022/03/27 17:55:40 by unix_user        ###   ########.fr       */
+/*   Updated: 2022/03/28 21:44:48 by unix_user        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
+	if (n == 0)
+		return (0);
 	while (*s1 == *s2)
 	{
 		if (!*s1 || !--n)
@@ -27,8 +29,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 
 // int	main(void)
 // {
-// 	printf("%d:%d\n", strncmp("ABC", "ABC", 4), ft_strncmp("ABC", "ABC", 4));
 // 	printf("%d:%d\n", strncmp("ABCD", "ABC", 3), ft_strncmp("ABCD", "ABC", 3));
 // 	printf("%d:%d\n", strncmp("ABCD", "ABC", 4), ft_strncmp("ABCD", "ABC", 4));
-// 	printf("%d:%d\n", strncmp("ABCD", "ABD", 3), ft_strncmp("ABCD", "ABD", 3));
+// 	printf("%d:%d\n", strncmp("ABC", "ABE", 10), ft_strncmp("ABC", "ABE", 10));
+// 	printf("%d:%d\n", strncmp("ABCD", "AC", 0), ft_strncmp("ABCD", "AC", 0));
+// 	printf("%d:%d\n", strncmp("", "", 1), ft_strncmp("", "", 1));
 // }
